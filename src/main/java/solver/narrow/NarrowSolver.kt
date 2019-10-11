99b3b736923acf667cb36762b0055bcfe8dfb39c
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2019-10-11 17:56:32 +0100

    makek server index long

--- src/main/java/solver/narrow/NarrowSolver.kt
+++ src/main/java/solver/narrow/NarrowSolver.kt
@@ -10,7 +10,16 @@ class NarrowSolver(override val name: String = "Narrow") : Solver {
     override fun solve(input: Input): Output {
         val compilationSteps = arrayListOf<CompilationStep>()
 
-        
+        val serverCount = input.servers
+
+        input.nodes
+            .forEach { (name, node) ->
+                for (i in 0 until serverCount) {
+                    if (node.dependencies.size <= 1) {
+                        compilationSteps.add(CompilationStep(name, i))
+                    }
+                }
+            }
 
         return Output(compilationSteps)
     }
