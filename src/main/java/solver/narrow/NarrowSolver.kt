83d9449a673dfea59ef8aa7350f02bb366d21398
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2019-10-11 18:07:51 +0100

    narrow solver

--- src/main/java/solver/narrow/NarrowSolver.kt
+++ src/main/java/solver/narrow/NarrowSolver.kt
@@ -4,6 +4,7 @@ import common.Input
 import common.Output
 import common.Solver
 import common.model.CompilationStep
+import common.model.FileNode
 
 class NarrowSolver(override val name: String = "Narrow") : Solver {
 
@@ -12,14 +13,53 @@ class NarrowSolver(override val name: String = "Narrow") : Solver {
 
         val serverCount = input.servers
 
+        val c = hashSetOf<FileNode>()
+
         input.nodes
-            .forEach { (name, node) ->
-                for (i in 0 until serverCount) {
-                    if (node.dependencies.size <= 1) {
-                        compilationSteps.add(CompilationStep(name, i))
-                    }
+            .forEach { (_, node) ->
+                if (node.dependencies.size > 1) { // 100 dependencies
+                    c.add(node)
                 }
             }
+
+        val q = hashSetOf<FileNode>()
+        val r = hashSetOf<FileNode>()
+        val a = hashSetOf<FileNode>()
+
+        c.forEach {
+            it.dependencies.forEach { cDependency ->
+                q.add(cDependency)
+
+                cDependency.dependencies.forEach { qDependency ->
+                    r.add(qDependency)
+                }
+            }
+        }
+
+        r.forEach {
+            it.dependencies.forEach {
+                a.add(it)
+            }
+        }
+
+        // add A
+        a
+            .forEach { node ->
+                for (i in 0 until serverCount) {
+                    compilationSteps.add(CompilationStep(node.name, i))
+                }
+            }
+
+        // add R
+        r
+            .forEach { node ->
+                for (i in 0 until serverCount) {
+                    compilationSteps.add(CompilationStep(node.name, i))
+                }
+            }
+
+        // TODO: add q evenly on servers by dependencies
+        // TODO: add c evenly on servers by dependencies
 
         return Output(compilationSteps)
     }
