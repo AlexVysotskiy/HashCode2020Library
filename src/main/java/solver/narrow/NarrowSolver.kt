c43341a686e4422db79b1f6ef55994c3edac02f5
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2019-10-18 18:25:35 +0100

    add random urgent solver

--- src/main/java/solver/narrow/NarrowSolver.kt
+++ src/main/java/solver/narrow/NarrowSolver.kt
@@ -5,6 +5,7 @@ import common.Output
 import common.Solver
 import common.model.CompilationStep
 import common.model.FileNode
+import solver.urgent.Server
 
 class NarrowSolver(override val name: String = "Narrow") : Solver {
 
@@ -19,6 +20,10 @@ class NarrowSolver(override val name: String = "Narrow") : Solver {
         val r = hashSetOf<FileNode>() // 50 "r"
         val a = hashSetOf<FileNode>() // 50 "a"
 
+        val servers = List(serverCount.toInt()) {
+            Server(it.toLong(), 0)
+        }
+
         input.nodes
             .values
             .filter { it.dependencies.size > 1 } // 50 "c" targets with 100 dependencies
@@ -27,7 +32,7 @@ class NarrowSolver(override val name: String = "Narrow") : Solver {
                 (input.targets[it.name]?.goal ?: -100) - depsSum
             }
             .reversed()
-            .take(35)
+            .take(34)
             .forEach { node -> c.add(node) }
 
         c.forEach {
@@ -51,6 +56,7 @@ class NarrowSolver(override val name: String = "Narrow") : Solver {
             .forEach { node ->
                 for (i in 0 until serverCount) {
                     compilationSteps.add(CompilationStep(node.name, i))
+                    servers.find { it.index == i }!!.currentCompilationSum += node.compilation
                 }
             }
 
@@ -59,20 +65,22 @@ class NarrowSolver(override val name: String = "Narrow") : Solver {
             .forEach { node ->
                 for (i in 0 until serverCount) {
                     compilationSteps.add(CompilationStep(node.name, i))
+                    servers.find { it.index == i }!!.currentCompilationSum += node.compilation
                 }
             }
 
         // round-robin all Qs
-        var currentServer = 0L
         q.forEach {
-            compilationSteps.add(CompilationStep(it.name, currentServer))
-            currentServer = (currentServer + 1) % serverCount
+            val server = servers.min()!!
+            server.currentCompilationSum += it.compilation
+            compilationSteps.add(CompilationStep(it.name, server.index))
         }
 
         // round-robin all Cs
         c.forEach {
-            compilationSteps.add(CompilationStep(it.name, currentServer))
-            currentServer = (currentServer + 1) % serverCount
+            val server = servers.min()!!
+            server.currentCompilationSum += it.compilation
+            compilationSteps.add(CompilationStep(it.name, server.index))
         }
 
         return Output(compilationSteps)
