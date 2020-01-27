9cc228ac27af210a55892fdc775b5f821d4f5ccd
Andrei Shikov <andrei.shikov@magiclab.co>
2020-01-27 19:11:17 +0000

    Add knapsack unique elements

--- src/main/java/solver/shikasd/KnapsackSolver.kt
+++ src/main/java/solver/shikasd/KnapsackSolver.kt
@@ -11,6 +11,15 @@ class KnapsackSolver(override val name: String = "shikasd.Knapsack"): Solver {
 
     override fun solve(input: Input): Output {
         val solution = Knapsack.solve(input.max.toInt(), input.slices) { CostValue(it.toLong(), it.toLong()) }
-        return Output(solution.map { input.slices.indexOf(it) })
+        val existing = arrayListOf<Int>()
+        return Output(solution.map {
+            var index = input.slices.indexOf(it)
+            while (existing.contains(index)) {
+                val newIndex = input.slices.subList(index + 1, input.slices.size).indexOf(it) + index + 1
+                index = newIndex
+            }
+            existing.add(index)
+            index
+        })
     }
 }
