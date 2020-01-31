ce7a88befe67f03be1864429f9876fb101168edb
Andrei Shikov <andrei.shikov@magiclab.co>
2020-01-31 17:29:39 +0000

    Add input format

--- src/main/java/common/Input.kt
+++ src/main/java/common/Input.kt
@@ -1,6 +1,26 @@
 package common
 
 data class Input(
-    val max: Long,
-    val slices: List<Int>
+    val gridSize: Size,
+    val vehicles: Int,
+    val rides: List<Ride>,
+    val bonus: Int,
+    val timeLimit: Int
+)
+
+data class Ride(
+    val start: Point,
+    val end: Point,
+    val startTime: Int,
+    val endTime: Int
+)
+
+data class Point(
+    val x: Int,
+    val y: Int
+)
+
+data class Size(
+    val width: Int,
+    val height: Int
 )
