3b8bff27ad00ee3615a228a589a708ed36297077
Andrei Shikov <andrei.shikov@magiclab.co>
2020-01-31 18:31:40 +0000

    Slow solver

--- src/main/java/solver/shikasd/GreedySolver.kt
+++ src/main/java/solver/shikasd/GreedySolver.kt
@@ -8,7 +8,7 @@ class GreedySolver(override val name: String = "shikasd.GreedySolver") : Solver 
         var tick = 0
         val carTakenUntil = IntArray(input.vehicles)
         val carPositions = Array(input.vehicles) { Point(0, 0) }
-        val sortedRides = input.rides.toMutableList()
+        val sortedRides = input.rides.mapIndexed { index, ride -> index to ride  }.toMutableList()
 
         val handledRides = mutableListOf<HandledRide>()
         while (tick < input.timeLimit) {
@@ -16,14 +16,15 @@ class GreedySolver(override val name: String = "shikasd.GreedySolver") : Solver 
                 if (time > tick) return@forEachIndexed
                 val carPosition = carPositions[index]
 
-                val rides = sortedRides.asSequence().filter { it.endTime >= it.distance() + carPosition.distanceTo(it.start) + tick }
-                val availableRide = rides.minBy { carPosition.distanceTo(it.start) } ?: return@forEachIndexed
+                val rides = sortedRides.asSequence().filter { (_, it) ->  it.endTime >= it.distance() + carPosition.distanceTo(it.start) + tick }
+                val availableRide = rides.minBy { carPosition.distanceTo(it.second.start) } ?: return@forEachIndexed
 
                 sortedRides.remove(availableRide)
-                carTakenUntil[index] = tick + carPosition.distanceTo(availableRide.start) + availableRide.distance()
+                carTakenUntil[index] = tick + carPosition.distanceTo(availableRide.second.start) + availableRide.second.distance()
                 handledRides.add(
-                    HandledRide(input.rides.indexOf(availableRide), index)
+                    HandledRide(availableRide.first, index)
                 )
+                carPositions[index] = availableRide.second.end
             }
             tick++
         }
