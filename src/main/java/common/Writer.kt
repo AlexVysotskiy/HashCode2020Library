a6326a405ecaa85e4dae4885eb508d0398a5b46e
Andrei Shikov <andrei.shikov@magiclab.co>
2020-02-20 18:10:47 +0000

    Add writer

--- src/main/java/common/Writer.kt
+++ src/main/java/common/Writer.kt
@@ -5,7 +5,27 @@ import java.io.OutputStream
 object Writer {
 
     fun write(output: Output, outputStream: OutputStream) {
+        val writer = outputStream.bufferedWriter()
 
+        writer.write(output.entries.size.toString())
+        writer.newLine()
+
+        output.entries.forEach {
+            writer.write(it.library.id.toString())
+            writer.write(" ")
+            writer.write(it.scannedBooks.size.toString())
+            writer.newLine()
+
+            it.scannedBooks.forEach {
+                writer.write(it.id.toString())
+                writer.write(" ")
+            }
+
+            writer.newLine()
+        }
+
+        writer.flush()
+        writer.close()
     }
 
 }
