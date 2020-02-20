047c4bba1bc1f395cdd508258d63df4d65887143
Andrei Shikov <andrei.shikov@magiclab.co>
2020-02-20 17:51:24 +0000

    Add input

--- src/main/java/common/Input.kt
+++ src/main/java/common/Input.kt
@@ -1,3 +1,18 @@
 package common
 
-class Input
+data class Input(
+    val days: Int,
+    val books: List<Book>,
+    val libraries: List<Library>
+)
+
+data class Book(
+    val id: Int,
+    val score: Int
+)
+
+data class Library(
+    val signup: Int,
+    val shippingRate: Int,
+    val books: List<Book>
+)
