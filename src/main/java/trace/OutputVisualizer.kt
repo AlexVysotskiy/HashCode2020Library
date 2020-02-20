0107830c8d7a3f59ed489fe142c5c6c6d0297ef7
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2020-02-20 19:07:07 +0000

    add trace

---
+++ src/main/java/trace/OutputVisualizer.kt
@@ -0,0 +1,85 @@
+package trace
+
+import java.util.concurrent.TimeUnit
+import common.Book
+import common.Input
+import common.Library
+import common.Output
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import java.io.File
+
+fun writeOutputVisualization(input: Input, output: Output, fileName: String) {
+    val traceEvents = mutableListOf<TraceEvent>()
+
+    var currentTime: Long = 0
+
+    val solvedBooks = hashSetOf<Book>()
+    val solvedLibraries = hashSetOf<Library>()
+
+    output.entries.forEach {
+        val library = it.library
+        val books = it.scannedBooks
+
+        solvedLibraries += library
+        currentTime += library.signup
+
+        traceEvents += TraceEvent(
+            pid = 0,
+            tid = library.id.toLong(),
+            ph = Phase.DurationStart,
+            name = library.id.toString() + "_signup",
+            ts = (currentTime - library.signup).toDouble(),
+            cname = "startup",
+            cat = "signup"
+        )
+        traceEvents += TraceEvent(
+            pid = 0,
+            tid = library.id.toLong(),
+            ph = Phase.DurationEnd,
+            name = library.id.toString() + "_signup",
+            ts = currentTime.toDouble()
+        )
+
+        var booksTime = currentTime
+        books
+            .chunked(library.shippingRate)
+            .forEach { booksThisDay ->
+                booksTime++
+
+                var scored = booksTime <= input.days
+                booksThisDay.forEach { book ->
+                    scored = scored && !solvedBooks.contains(book)
+                    traceEvents += TraceEvent(
+                        pid = 0,
+                        tid = library.id.toLong(),
+                        ph = Phase.DurationStart,
+                        name = book.id.toString(),
+                        ts = booksTime.toDouble() - 1,
+                        cname = if (scored) "good" else "bad",
+                        cat = if (scored) "scored" else "not_scored",
+                        args = mapOf(
+                            "score" to book.score.toString()
+                        )
+                    )
+                    traceEvents += TraceEvent(
+                        pid = 0,
+                        tid = library.id.toLong(),
+                        ph = Phase.DurationEnd,
+                        name = book.id.toString(),
+                        ts = booksTime.toDouble(),
+                        cname = if (scored) "good" else "bad"
+                    )
+                }
+            }
+
+
+        val traceRoot = TraceRoot(
+            traceEvents = traceEvents,
+            displayTimeUnit = TimeUnit.MILLISECONDS
+        )
+
+        val json = Json(JsonConfiguration.Default)
+        File(fileName).writeText(json.stringify(TraceRoot.serializer(), traceRoot))
+    }
+}
