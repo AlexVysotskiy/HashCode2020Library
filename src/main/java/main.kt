4b3e5411b174303a9dff802ccc845088d2c3a26c
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2020-02-20 18:03:23 +0000

    fix names

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -10,27 +10,27 @@ fun main(args: Array<String>) {
 
     val inputFiles = listOf(
         InputFile(
-            "inputs/a_example.in",
+            "inputs/a_example.txt",
             "5742876508028928"
         ),
         InputFile(
-            "inputs/b_read_on.in",
+            "inputs/b_read_on.txt",
             "5651204726063104"
         ),
         InputFile(
-            "inputs/c_incunabula.in",
+            "inputs/c_incunabula.txt",
             "5680006004998144"
         ),
         InputFile(
-            "inputs/d_tough_choices.in",
+            "inputs/d_tough_choices.txt",
             "5654843402223616"
         ),
         InputFile(
-            "inputs/e_so_many_books.in",
+            "inputs/e_so_many_books.txt",
             "5649463653695488"
         ),
         InputFile(
-            "inputs/f_libraries_of_the_world.in",
+            "inputs/f_libraries_of_the_world.txt",
             "5649463653695488"
         )
     )
