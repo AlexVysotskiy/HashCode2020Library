75839931f2aceb87e5ec1d9fa97dcea0065a70fd
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2020-02-20 18:20:18 +0000

    Add score calculator

--- src/main/java/common/score/kotlin/ScoreCalculatorImpl.kt
+++ src/main/java/common/score/kotlin/ScoreCalculatorImpl.kt
@@ -1,5 +1,6 @@
 package common.score.kotlin
 
+import common.Book
 import common.Input
 import common.Output
 import common.ScoreCalculator
@@ -8,6 +9,36 @@ import common.ScoreCalculator
 class ScoreCalculatorImpl : ScoreCalculator {
 
     override fun calculateResult(input: Input, output: Output): Long {
-        return 0
+        var result: Long = 0
+
+        var currentTime: Long = 0
+
+        val solvedBooks = hashSetOf<Book>()
+
+        output.entries.forEach {
+            val library = it.library
+            val books = it.scannedBooks
+
+            currentTime += library.signup
+
+            var booksTime = currentTime
+            books
+                .chunked(library.shippingRate)
+                .forEach { booksThisDay ->
+                    booksTime++
+
+                    if (booksTime < input.days) {
+                        booksThisDay.forEach { book ->
+                            if (!solvedBooks.contains(book)) {
+                                result += book.score
+                            } else {
+                                println("Warning: book ${book.id} was already scanned and we are trying to scan it again in library ${library.id}")
+                            }
+                        }
+                    }
+                }
+        }
+
+        return result
     }
 }
--- src/test/java/common/ResultCalculatorTest.kt
+++ src/test/java/common/ResultCalculatorTest.kt
@@ -1,7 +1,6 @@
 package common
 
 import common.score.kotlin.ScoreCalculatorImpl
-import common.score.kotlin.calculateScoreFast
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual
 import org.junit.Test
@@ -13,46 +12,33 @@ class ResultCalculatorTest {
     fun example() {
         val calculator = ScoreCalculatorImpl()
         val input = createInput()
-        val output = createOutput()
+        val output = createOutput(input)
 
         val result = calculator.calculateResult(input, output)
 
-        assertThat(result, IsEqual(10L))
-    }
-
-    @Test
-    fun exampleFast() {
-        val input = createInput()
-        val output = createOutput()
-
-        val result = calculateScoreFast(input, output.toFloatArray(input))
-
-        assertThat(result, IsEqual(10))
-    }
-
-    private fun Output.toFloatArray(input: Input): FloatArray {
-        val carFractions = FloatArray(input.vehicles) { it + 1.001f }
-        val initialPositions = FloatArray(input.rides.size)
-        handledRides.forEach {
-            initialPositions[it.rideIndex] = carFractions[it.vehicleIndex]
-            carFractions[it.vehicleIndex] += 0.001f
-        }
-        return initialPositions
+        assertThat(result, IsEqual(16L))
     }
 
     private fun createInput(): Input =
-        File("inputs/a_example.in")
+        File("inputs/a_example.txt")
             .inputStream()
             .use {
                 Reader.readInput(it)
             }
 
-    private fun createOutput(): Output {
+    private fun createOutput(input: Input): Output {
         return Output(
             listOf(
-                HandledRide(0, 0),
-                HandledRide(2, 1),
-                HandledRide(1, 1)
+                Entry(input.libraries[1], scannedBooks = listOf(input.books[5], input.books[2], input.books[3])),
+                Entry(
+                    input.libraries[0], scannedBooks = listOf(
+                        input.books[0],
+                        input.books[1],
+                        input.books[2],
+                        input.books[3],
+                        input.books[4]
+                    )
+                )
             )
         )
     }
